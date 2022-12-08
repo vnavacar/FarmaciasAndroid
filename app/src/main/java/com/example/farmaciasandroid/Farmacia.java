f554a51a82ed3c813a1ebc553d3cdbfb14557735
vnavacar <vnavacar@myuax.com>
2022-12-08 20:43:28 +0100

    farmacia

--- app/src/main/java/com/example/farmaciasandroid/Farmacia.java
+++ app/src/main/java/com/example/farmaciasandroid/Farmacia.java
@@ -8,110 +8,50 @@ public class Farmacia {
 
     private int id;
     private String titulo;
-    private String calle;
-    private String motivo;
-    private String tramo;
-    private String observaciones;
-    private Date inicio;
-    private Date fin;
+
+    private String telefono;
     private double latitud;
     private double longitud;
-    private String descripcion;
-    private String tipo;
+
+
+    public Farmacia(int id, String titulo, String telefono, double latitud, double longitud) {
+        this.id = id;
+        this.titulo = titulo;
+        this.telefono = telefono;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+    public Farmacia(){}
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getTitulo() {
         return titulo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
-    public String getCalle() {
-        return calle;
+    public String getTelefono() {
+        return telefono;
     }
-
-    public void setCalle(String calle) {
-        this.calle = calle;
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public String getTramo() {
-        return tramo;
-    }
-
-    public void setTramo(String tramo) {
-        this.tramo = tramo;
-    }
-
-    public String getObservaciones() {
-        return observaciones;
-    }
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
-    public Date getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(Date inicio) {
-        this.inicio = inicio;
-    }
-
-    public Date getFin() {
-        return fin;
-    }
-
-    public void setFin(Date fin) {
-        this.fin = fin;
-    }
-
     public double getLatitud() {
         return latitud;
     }
-
     public void setLatitud(double latitud) {
         this.latitud = latitud;
     }
-
     public double getLongitud() {
         return longitud;
     }
-
     public void setLongitud(double longitud) {
         this.longitud = longitud;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
